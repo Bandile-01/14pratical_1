5277eb70a63a008785f1da6ce84c16d5791c11f4
Bandile-01 <4543548@myuwc.ac.za>
2026-03-02 15:23:08 +0200

    Add comment to openchained.java

--- openchained.java
+++ openchained.java
@@ -10,6 +10,45 @@ public class openchained{
                 table[i]=-1;
             }
         }
+        private static int hash(int key){
+            return key % size;
+        }
+        public void insect(int item){
+            int index=hash(item);
+            while(table[index]!=-1){
+                index =(index +1)% size;
+
+            }
+            table[index]=item;
+
+
+
+        }
+        public static boolean search(int[]table,int item,int key){
+            int index=hash(key);
+            while(table[index]!=-1){
+                if(table[index]==item){
+                    return true;
+                }
+                index=(index +1)%size;
+
+            }
+            return false;
+
+        }
+        public void display(){
+            for(int i=0;i<size;i++){
+                if(table[i]==-1){
+                    System.out.println(i);
+
+                }else if(table[i]==-2){
+                    System.out.println(i);
+
+                }else{
+                    System.out.println(table[1]);
+                }
+            }
+        }
 
 
     }
